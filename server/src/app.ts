068522fbc87aa3e99e39dd899f1eb38c61a3ab4d
Chris Kurhan <chris.kurhan@dllgroup.com>
2022-10-26 17:58:21 -0500

    Clean up dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,8 @@
-import * as path from 'path';
 import app from './config/express';
 import routes from './routes/index.route';
-import { Request, Response } from 'express';
 
 // Router
 app.use('/api', routes);
-
-// Landing page
-app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 app.listen(app.get('port'), app.get('host'), () => {
     console.log(`Server running at http://${app.get('host')}:${app.get('port')}`);
